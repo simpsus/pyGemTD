a53c7026fb7ec631eb28974b3fe1b2e333a8deac
Bastian Kennel <bastian.kennel@gmail.com>
2020-02-22 17:54:02 +0100

    Hello World #creep

--- pyGemTD.py
+++ pyGemTD.py
@@ -23,6 +23,7 @@ colors = {
 # a square screen makes for maximum maze possibilities. 1k might be too much vertically, though
 width = 1000
 height = 1000
+tile_multiplier = 10
 
 display = pygame.display.set_mode((width,height))
 pygame.display.set_caption('pyGemTowerDefense')
@@ -31,6 +32,8 @@ arial_font = pygame.font.SysFont('arial',10)
 
 # render queue for background entities
 background = []
+# render queue for active creeps
+active_creeps = []
 
 # grid tile types
 FREE = 'F'
@@ -51,7 +54,7 @@ def cartesian_distance(one, other):
 	"""
 	geometric line distance between two points
 	"""
-	return math.sqrt((other.x - one.x)**2 + (other.y - one.y)**2)
+	return math.sqrt((other[0] - one[0])**2 + (other[1] - one[1])**2)
 
 def A_star(start, goal, h, d, ne):
 	"""
@@ -96,7 +99,7 @@ class Tile(object):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
 		background.append(self)
-		self.rect = pygame.Rect(x*10, y*10, 10, 10)
+		self.rect = pygame.Rect(x*tile_multiplier, y*tile_multiplier, tile_multiplier, tile_multiplier)
 		self.color = colors['ground']
 		self.type = FREE
 		self.x = x
@@ -127,19 +130,99 @@ class Tile(object):
 	def __repr__(self):
 		return '('+str(self.x) + ',' + str(self.y) + ')'
 
+class Creep(object):
+
+	def __init__(self, hp, speed, creep_type):
+		self.pos = None
+		self.hp = hp
+		# size is the radius of the circle that represents the creep
+		#TODO change creeps to animated sprites
+		self.size = 4
+		self.currnet_hp = self.hp
+		self.speed = speed
+		self.current_speed = self.speed
+		# creep types can either be things like immune/fast/boss/group
+		#TODO decide how flying and potentially other types (spawn) are handled. type or sublcass
+		self.type = creep_type
+		self.rect = None
+		#TODO stun flag/timer, slowed flag/timer/amount
+		# when a creep is active it is on the map and walks
+		self.active = False
+		# when a creep is breached it made it to the end alive
+		self.breached = False
+		self.path = []
+		#the point currently walking towards
+		self.current_destination = None
+		self.dead = False
+
+	def activate(self):
+		self.active = True
+		self.pos = (round(self.path[0].x * tile_multiplier + 0.5 * tile_multiplier), \
+			round(self.path[0].y * tile_multiplier + 0.5 * tile_multiplier))
+		self.current_destination = (round(self.path[1].x * tile_multiplier + 0.5 * tile_multiplier), \
+			round(self.path[1].y * tile_multiplier + 0.5 * tile_multiplier))
+		self.path = self.path[2:]
+
+	def die(self):
+		self.active = False
+
+	def breach(self):
+		self.breached = True
+		self.active = False
+
+	def update(self):
+		# update means the creep is active, so it is walking
+		#TODO implement things like stunned and slowed
+		#pos and current_destination are tuples, not objects
+		#the coordinates of the path have to be translated to screen coordinates
+		#logger.debug('Creep Position before update: ' + str(self.pos) + '. Destination ' + str(self.current_destination))
+		if cartesian_distance(self.pos, self.current_destination) <= self.speed:
+			if len(self.path) == 0:
+				self.breach()
+				return
+			else:
+				self.current_destination = (round(self.path[0].x * tile_multiplier + 0.5 * tile_multiplier), \
+					round(self.path[0].y * tile_multiplier + 0.5 * tile_multiplier))
+				self.path = [] if len(self.path) == 1 else self.path[1:]
+		#now we walk. x first, then y
+		if abs(self.pos[0] - self.current_destination[0]) > self.speed:
+			if self.pos[0] < self.current_destination[0]:
+				#we are walking right
+				self.pos = (self.pos[0] + self.speed, self.pos[1])
+			else:
+				#we are walking left
+				self.pos = (self.pos[0] - self.speed, self.pos[1])
+		else:
+			#as we are active so we are not breached, y it has to be
+			if self.pos[1] < self.current_destination[1]:
+				#we are walking down
+				self.pos = (self.pos[0], self.pos[1] + self.speed)
+			else:
+				#we are walking up
+				self.pos = (self.pos[0], self.pos[1] - self.speed)
+		#logger.debug('Creep Position after update: ' + str(self.pos) + '. Destination ' + str(self.current_destination))
+
+	def draw(self, surface):
+		pygame.draw.circle(surface, colors['creep'], self.pos, self.size, 0)
+
+	def __repr__(self):
+		return 'Creep(' + str(id(self)) + ')@' + str(self.pos)
+
 class Game(object):
 
 	def __init__(self):
 		# initialize the grid with Tiles
 		self.grid = {}
-		for x in range(width//10):
-			for y in range(height//10):
+		for x in range(width//tile_multiplier):
+			for y in range(height//tile_multiplier):
 				self.grid[(x,y)] = Tile(x,y)
 		# define the waypoints
 		self.start = (0,5)
 		self.waypoints = [(10,5),(10,50),(90,50),(90,5),(50,5),(50,80)]
 		self.end = (99,80)
 		self.path = []
+		# the creeps currently in the game. TODO change to (to be implemented) Wave
+		self.current_creeps = []
 
 	def show_waypoints(self):
 		"""
@@ -177,8 +260,8 @@ class Game(object):
 				A_star(
 					a,
 					b,
-					lambda w: cartesian_distance(a,w),
-					lambda w1,w2: cartesian_distance(w1,w2),
+					lambda w: cartesian_distance((a.x, a.y),(w.x, w.y)),
+					lambda w1,w2: cartesian_distance((w1.x, w1.y),(w2.x,w2.y)),
 					lambda w: self.get_neighbor(w)
 					)
 				)
@@ -199,14 +282,35 @@ class Game(object):
 				tile.clear()
 
 	def get_tile_for_position(self, pos):
-		return self.grid[(pos[0]//10,pos[1]//10)]
+		return self.grid[(pos[0]//tile_multiplier,pos[1]//tile_multiplier)]
+
+	def process_breach(self, creep):
+		logger.debug('Creep breached: ' + str(creep))
+		self.current_creeps.remove(creep)
+		active_creeps.remove(c)
+		logger.debug('Creep ' + str(c) + ' removed from active creeps.')
+
+
+	def update(self):
+		#update the list of creeps
+		for c in self.current_creeps:
+			c.update()
+			if c.breached:
+				self.process_breach(c)
 
 game = Game()
 game.make_path()
 game.show_waypoints()
 game.show_path()
+logger.debug('Creating and activating test creep')
+creep = Creep(100,2,'NORMAL')
+creep.path = game.path[:]
+creep.activate()
+game.current_creeps.append(creep)
+active_creeps.append(creep)
+logger.debug('Active Creeps ' + str(active_creeps))
 terminated = False
-logging.debug('Space triggers a path update. Left mouse button to block a tile.')
+#Path updates will not affect the debug creep
 while not terminated:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -223,8 +327,11 @@ while not terminated:
 				tile.clear()
 			elif tile.type != WAYPOINT:
 				tile.block()
+	game.update()
 	for b in background:
 		b.draw(display)
+	for c in active_creeps:
+		c.draw(display)
 	pygame.display.update()
 	clock.tick(60)
 
